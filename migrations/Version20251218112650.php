5d5319deff6fc8f7d1a35dd35b6ab608d684d0a6
Alexandr Belotsitsko <sanyabelyy020@gmail.com>
2025-12-18 14:47:59 +0600

    added actual migration

--- migrations/Version20251218112650.php
+++ migrations/Version20251218112650.php
@@ -3,11 +3,11 @@
 namespace Sprint\Migration;
 
 
-class Version20251117100501 extends Version
+class Version20251218112650 extends Version
 {
     protected $author = "admin";
 
-    protected $description = "миграция для инфоблока отзывов";
+    protected $description = "инфоблок отзывы";
 
     protected $moduleVersion = "5.4.1";
 
@@ -405,6 +405,54 @@ class Version20251117100501 extends Version
   'administrators' => 'X',
 ));
         $helper->Iblock()->saveProperty($iblockId, array (
+  'NAME' => 'Товар',
+  'ACTIVE' => 'Y',
+  'SORT' => '100',
+  'CODE' => 'PRODUCT',
+  'DEFAULT_VALUE' => '',
+  'PROPERTY_TYPE' => 'E',
+  'ROW_COUNT' => '1',
+  'COL_COUNT' => '30',
+  'LIST_TYPE' => 'L',
+  'MULTIPLE' => 'N',
+  'XML_ID' => NULL,
+  'FILE_TYPE' => '',
+  'MULTIPLE_CNT' => '5',
+  'LINK_IBLOCK_ID' => 'catalog:catalog',
+  'WITH_DESCRIPTION' => 'N',
+  'SEARCHABLE' => 'N',
+  'FILTRABLE' => 'N',
+  'IS_REQUIRED' => 'N',
+  'VERSION' => '1',
+  'USER_TYPE' => NULL,
+  'USER_TYPE_SETTINGS' => 'a:0:{}',
+  'HINT' => '',
+));
+            $helper->Iblock()->saveProperty($iblockId, array (
+  'NAME' => 'Предложение',
+  'ACTIVE' => 'Y',
+  'SORT' => '100',
+  'CODE' => 'OFFER',
+  'DEFAULT_VALUE' => '',
+  'PROPERTY_TYPE' => 'E',
+  'ROW_COUNT' => '1',
+  'COL_COUNT' => '30',
+  'LIST_TYPE' => 'L',
+  'MULTIPLE' => 'N',
+  'XML_ID' => NULL,
+  'FILE_TYPE' => '',
+  'MULTIPLE_CNT' => '5',
+  'LINK_IBLOCK_ID' => '0',
+  'WITH_DESCRIPTION' => 'N',
+  'SEARCHABLE' => 'N',
+  'FILTRABLE' => 'N',
+  'IS_REQUIRED' => 'N',
+  'VERSION' => '1',
+  'USER_TYPE' => NULL,
+  'USER_TYPE_SETTINGS' => 'a:0:{}',
+  'HINT' => '',
+));
+            $helper->Iblock()->saveProperty($iblockId, array (
   'NAME' => 'Пользователь',
   'ACTIVE' => 'Y',
   'SORT' => '100',
@@ -584,54 +632,6 @@ class Version20251117100501 extends Version
   'VERSION' => '1',
   'USER_TYPE' => NULL,
   'USER_TYPE_SETTINGS' => 'a:0:{}',
-  'HINT' => '',
-));
-            $helper->Iblock()->saveProperty($iblockId, array (
-  'NAME' => 'Товар',
-  'ACTIVE' => 'Y',
-  'SORT' => '500',
-  'CODE' => 'PRODUCT',
-  'DEFAULT_VALUE' => '',
-  'PROPERTY_TYPE' => 'S',
-  'ROW_COUNT' => '1',
-  'COL_COUNT' => '30',
-  'LIST_TYPE' => 'L',
-  'MULTIPLE' => 'N',
-  'XML_ID' => NULL,
-  'FILE_TYPE' => '',
-  'MULTIPLE_CNT' => '5',
-  'LINK_IBLOCK_ID' => '0',
-  'WITH_DESCRIPTION' => 'N',
-  'SEARCHABLE' => 'N',
-  'FILTRABLE' => 'N',
-  'IS_REQUIRED' => 'N',
-  'VERSION' => '1',
-  'USER_TYPE' => NULL,
-  'USER_TYPE_SETTINGS' => NULL,
-  'HINT' => '',
-));
-            $helper->Iblock()->saveProperty($iblockId, array (
-  'NAME' => 'Предложение',
-  'ACTIVE' => 'Y',
-  'SORT' => '500',
-  'CODE' => 'OFFER',
-  'DEFAULT_VALUE' => '',
-  'PROPERTY_TYPE' => 'S',
-  'ROW_COUNT' => '1',
-  'COL_COUNT' => '30',
-  'LIST_TYPE' => 'L',
-  'MULTIPLE' => 'N',
-  'XML_ID' => NULL,
-  'FILE_TYPE' => '',
-  'MULTIPLE_CNT' => '5',
-  'LINK_IBLOCK_ID' => '0',
-  'WITH_DESCRIPTION' => 'N',
-  'SEARCHABLE' => 'N',
-  'FILTRABLE' => 'N',
-  'IS_REQUIRED' => 'N',
-  'VERSION' => '1',
-  'USER_TYPE' => NULL,
-  'USER_TYPE_SETTINGS' => NULL,
   'HINT' => '',
 ));
         $helper->UserOptions()->saveElementGrid($iblockId, array (
